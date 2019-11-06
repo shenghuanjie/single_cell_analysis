554c89ac11ac2305329532e1a267a50bb9865eda
shenghuanjie <shenghuanjie@berkeley.edu>
2019-11-06 10:47:15 -0800

    lazy commit

---
+++ sc_workflow.py
@@ -0,0 +1,21 @@
+"""
+
+"""
+import scanpy as sc
+
+
+"""
+Autoencoder -> out-of-sample prediction (linear combination in the latent space)
+"""
+
+
+"""
+Differential Expression Analysis
+__author__: davidsebfischer
+
+GLM based
+Wald tests and likelihood ratio tests
+Correct for batch effect linearly
+
+Bayesian model -> scVI framework
+"""
